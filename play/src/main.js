34089fe44edfdc1aa8e04eae9d5b808801bcb3b3
xmf <501235949@qq.com>
2023-05-19 16:43:12 +0800

    chore: 新增文档的部署命令

--- play/src/main.js
+++ play/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import 'element-plus/dist/index.css'
 import 'ant-design-vue/dist/antd.min.css'
-import { DRender } from 'd-render'
+import { DRender, version } from 'd-render'
 import 'd-render/dist/index.css'
 import dRenderConfig from '../d-render.config'
 
+console.log('d-render', version)
 const dRender = new DRender()
 dRender.setConfig(dRenderConfig)
 createApp(App).use(router).mount('#app')
