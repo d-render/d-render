c2f5255dc9beebd047c3f157934fc7560efebcc2
xmf <501235949@qq.com>
2023-05-19 18:04:32 +0800

    chore(docs): 修改html的title

--- play/vue.config.js
+++ play/vue.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   // transpileDependencies: true
   chainWebpack: (config) => {
     config.module.rule('md').test(/\.md$/).use().loader(path.resolve(__dirname, './loaders/markdown-loader.js')).end()
+    config.plugin('html').tap(args => {
+      args[0].title = 'DRender Docs'
+      return args
+    })
     return config
   }
 })
