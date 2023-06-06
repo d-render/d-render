a828b65692efd2f4c0e5792003c0a3ed4af05e89
xmf <501235949@qq.com>
2023-06-06 13:42:29 +0800

    fix(shared): 修复use-dom-resize在开发环境的报错

--- packages/shared/src/hooks/use-dom-resize.js
+++ packages/shared/src/hooks/use-dom-resize.js
@@ -1,22 +1,41 @@
-import { onMounted, onUnmounted } from 'vue'
-import { debounce } from '../utils'
-
+import { onMounted, onUnmounted, onActivated, onDeactivated } from 'vue'
+class OResizeObserver extends window.ResizeObserver {
+  constructor (cb) {
+    super((entries, observe) => {
+      window.requestAnimationFrame(() => {
+        cb(entries, observe)
+      })
+    })
+  }
+}
 export const useObserveDomResize = (container, cb) => {
   onMounted(() => {
     const domList = [].concat(container()) // 同时支持数组及单个容器
     const cbList = [].concat(cb)
-    const resizeObserver = new window.ResizeObserver(debounce((entries) => requestAnimationFrame(() => {
+    const resizeObserver = new OResizeObserver((entries) => {
       entries.forEach((entry, i) => {
         const cb = cbList[i]
         if (cb && typeof cb === 'function') {
           cb(entry)
         }
       })
-    })), 16)
+    })
     // 开启监听
     domList.forEach((dom) => {
       resizeObserver.observe(dom)
     })
+    onActivated(() => {
+      domList.forEach((dom) => {
+        resizeObserver.observe(dom)
+      })
+    })
+    onDeactivated(() => {
+      console.log('unobserve')
+      domList.forEach((dom) => {
+        resizeObserver.unobserve(dom)
+      })
+    })
+
     onUnmounted(() => {
       // 销毁
       resizeObserver.disconnect()
