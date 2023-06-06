b0181defa0cc291eae115377cb5e9c0cb1da68aa
xmf <501235949@qq.com>
2023-06-06 10:37:23 +0800

    fix(use-dom-resize): 初步修复ResizeObserver loop limit exceeded的问题

--- packages/shared/src/hooks/use-dom-resize.js
+++ packages/shared/src/hooks/use-dom-resize.js
@@ -1,17 +1,18 @@
 import { onMounted, onUnmounted } from 'vue'
+import { debounce } from '../utils'
 
 export const useObserveDomResize = (container, cb) => {
   onMounted(() => {
     const domList = [].concat(container()) // 同时支持数组及单个容器
     const cbList = [].concat(cb)
-    const resizeObserver = new ResizeObserver((entries)=> requestAnimationFrame(() => {
+    const resizeObserver = new window.ResizeObserver(debounce((entries) => requestAnimationFrame(() => {
       entries.forEach((entry, i) => {
         const cb = cbList[i]
         if (cb && typeof cb === 'function') {
           cb(entry)
         }
       })
-    }))
+    })), 16)
     // 开启监听
     domList.forEach((dom) => {
       resizeObserver.observe(dom)
