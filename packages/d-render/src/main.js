1104ac3ae946da2200ef79e2a3dd411583882c2b
xmf <501235949@qq.com>
2023-06-27 09:28:52 +0800

    refactor(d-render): 将不输入项目的样式移除，导出以Dr为前缀的组件

--- packages/d-render/src/main.js
+++ packages/d-render/src/main.js
@@ -1,12 +1,13 @@
 import pkg from '../package.json'
 export const version = pkg.version
-export { default as CipForm } from './cip-form'
-export { default as CipFormItem } from './cip-form-item'
-export { default as CipFormLayout } from './cip-form-layout'
-export { default as CipSearchForm } from './cip-search-form'
-export { default as CipTable } from './cip-table'
-export { default as CipFormRender } from './cip-form-render'
-export { default as CipFormInputTransform } from './cip-form-input-transform'
+export { default as CipForm, default as DrForm } from './cip-form'
+export { default as CipFormItem, default as DrFormItem } from './cip-form-item'
+export { default as CipFormLayout, default as DrFormLayout } from './cip-form-layout'
+export { default as CipSearchForm, default as DrSearchForm } from './cip-search-form'
+export { default as CipTable, default as DrTable } from './cip-table'
+export { default as CipFormRender, default as DrFormRender } from './cip-form-render'
+export { default as CipFormInputTransform, default as DrFormInputTransform } from './cip-form-input-transform'
+
 export {
   DRender,
   settingValueTransformState,
