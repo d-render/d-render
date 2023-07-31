ea9c058cc37f479d3cce91529e55644e8e33fa08
xmf <39474671+d-render@users.noreply.github.com>
2023-07-31 15:02:59 +0800

    docs(shared): 完善useFormInput的类型

--- packages/shared/types/hooks/use-form-input.d.ts
+++ packages/shared/types/hooks/use-form-input.d.ts
@@ -26,7 +26,7 @@ export function useOptions(
   props: IAnyObject,
   multiple: Ref<boolean>|boolean,
   updateStream: UpdateFormStream,
-  context:SetupContext
+  context?:SetupContext
 ): {
   optionProps: ComputedRef<{label: string, value: string, children?: string} & IAnyObject>
   options: Ref<Array<string|number|IAnyObject>>
@@ -38,7 +38,7 @@ export function useOptions(
   splitKey: ComputedRef<string>
   proxyOptionsValue: ComputedRef<any>
 }
-export type TKeyMap = 'string'| ['string', 'string'] | ['string', {key: string, default?: any}|{key?:string, default: any}]
+export type TKeyMap = string | [string, string] | [string, {key: string, default?: any}|{key?:string, default: any}]
 export function useInputProps(
   props: IAnyObject,
   propKeys: Array<TKeyMap>
