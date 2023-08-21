2bad8cb756dd75ba12cda82267ec133fa6896ae1
xmf <39474671+d-render@users.noreply.github.com>
2023-08-21 15:46:35 +0800

    fix(task): 修复types在es下无法打包的问题

--- build/tasks/types.js
+++ build/tasks/types.js
@@ -1,13 +1,19 @@
 /**
  * @description 此文件用于生成按需引入的组件
  */
-require('@esbuild-kit/cjs-loader')
-const { buildDirResolve } = require("build/utils/path")
-console.log('buildDirResolve',buildDirResolve)
-const chalk = require('chalk')
-const fglob = require('fast-glob')
-const { writeFileSync } = require('fs-extra')
-const entry =  process.env.ENTRY_MODULE || 'cip-styles'
+// require('@esbuild-kit/cjs-loader')
+import { buildDirResolve } from '../utils/path.js'
+import '@esbuild-kit/cjs-loader'
+import chalk from 'chalk'
+import fglob from 'fast-glob'
+import FsExtra from 'fs-extra'
+const { writeFileSync } = FsExtra
+// const { buildDirResolve } = require('../utils/path')
+console.log('buildDirResolve', buildDirResolve)
+// const chalk = require('chalk')
+// const fglob = require('fast-glob')
+// const { writeFileSync } = require('fs-extra')
+const entry = process.env.ENTRY_MODULE || 'cip-styles'
 console.log('types entry', entry)
 const packageBase = buildDirResolve(`../packages/${entry}`)
 const dirs = fglob.sync(`${packageBase}/src/**/component.scheme.js`)
@@ -62,7 +68,7 @@ const genComponentsDTs = () => {
         }, []).join('\n')
         const slotsScheme = componentScheme.slotsScheme
         const slotsLen = Object.keys(slotsScheme || {}).length
-        const slotsString =  Object.keys(slotsScheme || {}).reduce((acc, key) => {
+        const slotsString = Object.keys(slotsScheme || {}).reduce((acc, key) => {
           const type = JSON.stringify(slotsScheme[key] || {})
           acc.push(`  ${formatKey(key)}?: ${type}`)
           return acc
@@ -70,7 +76,7 @@ const genComponentsDTs = () => {
 
         const componentName = componentScheme.name || 'Component'
         const content = `import { CustomComponent, IAnyObject } from '@xdp/types'
-export const ${componentName}: CustomComponent<{\n${propsString}\n}, {\n${emitsString}\n}${ slotsLen > 0 ? ',{\n'+slotsString+'\n}' : '' }>
+export const ${componentName}: CustomComponent<{\n${propsString}\n}, {\n${emitsString}\n}${slotsLen > 0 ? ',{\n' + slotsString + '\n}' : ''}>
 export default ${componentName}
 `
         const filename = dir.replace(packageBase, '').replace('/src', '').replace('/component.scheme.js', '')
@@ -92,13 +98,13 @@ export default ${componentName}
 // 生成main.d.ts类型
 genComponentsDTs().then(res => {
   let mainContent = ''
-  if(Object.keys(componentDTs).length > 1){
+  if (Object.keys(componentDTs).length > 1) {
     mainContent = `${Object.keys(componentDTs).reduce((acc, key) => {
       acc.push(`export { ${key} } from '${componentDTs[key]}'`)
       return acc
     }, []).join('\n')}
 `
-  }else{
+  } else {
     mainContent = `${Object.keys(componentDTs).reduce((acc, key) => {
       acc.push(`export { ${key} as default } from '${componentDTs[key]}'`)
       return acc
