5058d0ec3ef513cd708889985945fab5d5648d6d
Sheeran Xu <sheeran.xu@outlook.com>
2023-08-23 14:15:06 +0800

    feat(design): 新增schema结构小组件
    * polish(优化左侧树): 优化左侧树
    
    * polish(优化左侧树): 优化左侧树
    
    * polish(标题格式化): 左侧树标题格式化
    
    ---------
    
    Co-authored-by: xuhl <xuhl@citycloud.com.cb>

--- play/src/views/palyground/form-design/index.jsx
+++ play/src/views/palyground/form-design/index.jsx
@@ -3,10 +3,13 @@ import '@d-render/design/dist/index.css'
 import PlInfo from '@cip/components/page-layout/info'
 import { componentsGroupList } from './config'
 import { ref } from 'vue'
+import schemaJson from './schema'
+
 export default {
   setup () {
     const schema = ref({})
     const equipment = ref('pc')
+    schema.value = schemaJson
     return () => <PlInfo>
       <DrFormDesign
         style={'background: #fff'}
