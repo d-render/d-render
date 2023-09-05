592935724997c21664b82b2a854e4900b0c69658
SheeranX <sheeran.xu@outlook.com>
2023-09-05 16:18:48 +0800

    polish(表格渲染): 优化预览表格样式

--- packages/design/src/plugins/table-preview/component.jsx
+++ packages/design/src/plugins/table-preview/component.jsx
@@ -1,4 +1,7 @@
 import { CipTable } from 'd-render'
+import { CipButtonText } from '@xdp/button'
+import { Plus } from '@element-plus/icons-vue'
+
 export default {
   props: {
     schema: {},
@@ -9,14 +12,30 @@ export default {
   setup (props, { emit }) {
     // 初始化model
     emit('update:model', [])
+    const handleAdd = () => {
+      emit('update:model', [...props.model, {}])
+    }
     return () => {
       const { list = [], ...tableConfig } = props.schema || {}
-      return <CipTable
-        {...tableConfig}
-        data={props.model}
-        onUpdate:data={(val) => emit('update:model', val)}
-        columns={list}
-      />
+      return <div class={'dr-table-preview--table'}>
+        <CipTable
+          {...tableConfig}
+          withTableHandle={true}
+          data={props.model}
+          onUpdate:data={(val) => emit('update:model', val)}
+          columns={list}
+          v-slots={
+            {
+              $handler: () => <div class={'dr-table-preview--handler'}>
+                <CipButtonText>新增</CipButtonText>
+                <CipButtonText>删除</CipButtonText>
+              </div>
+            }
+          }
+        >
+        </CipTable>
+        <CipButtonText icon={Plus} onClick={handleAdd}>添加</CipButtonText>
+      </div>
     }
   }
 }
