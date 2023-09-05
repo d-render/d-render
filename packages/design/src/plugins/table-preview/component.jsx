701266d93df99ff22b5fc837946d4c75f8fdb699
SheeranX <sheeran.xu@outlook.com>
2023-09-05 19:12:44 +0800

    polish(表格渲染): 优化预览表格样式

--- packages/design/src/plugins/table-preview/component.jsx
+++ packages/design/src/plugins/table-preview/component.jsx
@@ -1,4 +1,4 @@
-import { CipTable } from 'd-render'
+import {CipForm, CipFormItem, CipTable} from 'd-render'
 import { CipButtonText } from '@xdp/button'
 import { Plus } from '@element-plus/icons-vue'
 
@@ -15,9 +15,31 @@ export default {
     const handleAdd = () => {
       emit('update:model', [...props.model, {}])
     }
+    const handleDelete = () => {
+      emit('update:model', props.model.filter((_, index) => index !== props.model.length - 1))
+    }
     return () => {
+      console.log(props.model, 'props.model')
       const { list = [], ...tableConfig } = props.schema || {}
-      return <div class={'dr-table-preview--table'}>
+      const mobile = () => {
+        return <CipForm fieldList={[]} readonly={true}>
+          {
+            list?.map((option, index) => <CipFormItem
+              key={index}
+              model={props.model[index]}
+              onUpdate:model={(val) => {
+                const modelList = [...props.model]
+                modelList[index] = val
+                console.log(modelList, 'modelList', val)
+                emit('update:model', modelList)
+              }}
+              config={option.config}
+              inTable={true}
+            ></CipFormItem>)
+          }
+        </CipForm>
+      }
+      const pc = () => <div class={'dr-table-preview--table'}>
         <CipTable
           {...tableConfig}
           withTableHandle={true}
@@ -27,8 +49,8 @@ export default {
           v-slots={
             {
               $handler: () => <div class={'dr-table-preview--handler'}>
-                <CipButtonText>新增</CipButtonText>
-                <CipButtonText>删除</CipButtonText>
+                <CipButtonText onClick={handleAdd}>新增</CipButtonText>
+                <CipButtonText onClick={handleDelete}>删除</CipButtonText>
               </div>
             }
           }
@@ -36,6 +58,7 @@ export default {
         </CipTable>
         <CipButtonText icon={Plus} onClick={handleAdd}>添加</CipButtonText>
       </div>
+      return props.equipment === 'pc' ? pc() : mobile()
     }
   }
 }
