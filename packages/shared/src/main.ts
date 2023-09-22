0deb0da4aecbfac6b534a9e93fcbaa32660ee651
Lsraphel <501235949@qq.com>
2023-09-22 10:31:20 +0800

    fix(shared): [main]导出CustomComponent类型

--- packages/shared/src/main.ts
+++ packages/shared/src/main.ts
@@ -1,3 +1,18 @@
+import { Component, Slot } from 'vue'
 export * from './helper/index'
 export * from './hooks/index'
 export * from './utils/index'
+type On<T> = {
+  [K in keyof T as `on${Capitalize<string & K>}`]: T[K]
+}
+type Slots<T> = {
+  'v-slots'?: {
+    [K in keyof T]?: (arg0: T[K]) => Slot
+  }
+  children?: {
+    [K in keyof T]?: (arg0: T[K]) => Slot
+  }
+}
+
+// vue3 component jsx的辅助类型
+export declare type CustomComponent<T=any,M=any, S=any> = new (Props: {[K in keyof T]: T[K]} & On<M> & Slots<S>) => Component<{[K in keyof T]: T[K]} & On<M> & Slots<S>>
