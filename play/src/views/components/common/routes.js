91e5c0a978a5b9452e0f86de4f76440868fa2dc8
Lsraphel <501235949@qq.com>
2023-09-27 18:59:31 +0800

    polish: 开放changeValue

--- play/src/views/components/common/routes.js
+++ play/src/views/components/common/routes.js
@@ -1,9 +1,9 @@
 export const routes = [
-  // {
-  //   path: '/common/changeValue',
-  //   name: 'commonChangeValue',
-  //   component: () => import('./change-value')
-  // },
+  {
+    path: '/common/changeValue',
+    name: 'commonChangeValue',
+    component: () => import('./change-value')
+  },
   {
     path: '/common/changeConfig',
     name: 'commonChangeConfig',
