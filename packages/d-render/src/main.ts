a28a1e4fc91b5d28c33b413d435419a0c9dcf493
Lsraphel <501235949@qq.com>
2026-05-10 10:26:58 +0800

    fix: enable JSON module resolution and update version import method
    - Added "resolveJsonModule" to tsconfig for JSON imports.
    - Changed version export to dynamically pull from package.json.

--- packages/d-render/src/main.ts
+++ packages/d-render/src/main.ts
@@ -1,5 +1,5 @@
-// import pkg from '../package.json'
-export const version = '2.1.14'// pkg.version
+import pkg from '../package.json'
+export const version = pkg.version
 export { default as CipForm, default as DrForm } from './cip-form'
 export { default as CipFormItem, default as DrFormItem } from './cip-form-item'
 export { default as CipFormLayout, default as DrFormLayout } from './cip-form-layout'
